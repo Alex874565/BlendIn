0d6e246c1fef78130c8b7c2784145b29605ceef6
Alex874565 <alex.carpinisan@gmail.com>
2025-06-20 18:46:19 +0300

    SFX

--- Assets/Assets/Scriptables/ColorStatesDatabase.cs
+++ Assets/Assets/Scriptables/ColorStatesDatabase.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ColorStatesDatabase", menuName = "ScriptableObjects/ColorStatesDatabase", order = 1)]
--- Assets/Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Assets/Scripts/PlayerBehaviour.cs
@@ -1,8 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.Animations;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
